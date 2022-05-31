edab2594b0a4351db077968225bb9004cfe14863
Denis Kravchuk <deniskrv@gmail.com>
2022-05-31 20:45:06 +0300

    add delete method

--- routes/todo.js
+++ routes/todo.js
@@ -47,9 +47,15 @@ router.put('/:id', async (req, res) => {
 })
 
 //delete todos
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-
+        const todos = await Todo.findAll({
+            where: {
+                id: +req.params.id
+            }
+        })
+        await todos[0].destroy()
+        res.status(204).json({})
     } catch (e) {
         console.log(e)
         res.status(500).json({
